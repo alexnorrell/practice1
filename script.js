705e4b4cf5cb9e122c474fc329a1fa1115bda275
alexnorrell <norrell.alex@gmail.com>
2023-01-08 19:49:08 -0500

    working on variables and if's

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+var upperArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numberArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9" ];
+var characterArr = ["~", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", ";", ":", "'", "?", "<", ">", ",", ".", "/"];
+var passwordLength = [];
+var choiceArr =[]
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -15,4 +22,14 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
-// texting
+function getPrompt(){
+  characterLength = parseInt(prompt("How many characters would you like your password to be? It must be between 8 and 128 characters."));
+
+  if(isNaN(characterLength) || characterLength < 8 || characterLength > 128){
+    alert("Your password must be between 8 and 128 characters long. Try again.");
+  }
+
+  if(){
+    
+  }
+}
